64c6f3c4fc76a963ce98fe82e57c31d5301b45f0
Tobias Salzmann <796084+Eun@users.noreply.github.com>
2023-11-14 18:01:54 +0100

    invert case

---
+++ modules/string/invert_case.mod.js
@@ -0,0 +1,17 @@
+module = {
+    name: 'invert case',
+    description: 'invert the case of a string',
+    categories: ['string'],
+    properties: [],
+    run: function (str) {
+        for (var i = 0; i < str.length; i++) {
+            var char = str.charAt(i);
+            if (char === char.toUpperCase()) {
+                str = str.substring(0, i) + char.toLowerCase() + str.substring(i + 1);
+            } else {
+                str = str.substring(0, i) + char.toUpperCase() + str.substring(i + 1);
+            }
+        }
+        return str;
+    }
+}
