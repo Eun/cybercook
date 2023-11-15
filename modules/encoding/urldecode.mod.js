6bd6643925a59dc8232c42b18b6e0356146f1713
Tobias Salzmann <796084+Eun@users.noreply.github.com>
2023-11-15 09:14:37 +0100

    commit to specified branch

--- modules/encoding/urldecode.mod.js
+++ modules/encoding/urldecode.mod.js
@@ -1,6 +1,6 @@
 module = {
     name: 'urldecode',
-    description: 'url decode an string',
+    description: 'url decode the input',
     categories: ['string', 'url'],
     properties: [],
     run: function (text) {
