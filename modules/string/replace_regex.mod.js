94b2ac0aa39459b0efe1a0a2b9f0ed085c735ba4
Tobias Salzmann <796084+Eun@users.noreply.github.com>
2023-12-05 16:31:43 +0100

    replace with regex

---
+++ modules/string/replace_regex.mod.js
@@ -0,0 +1,26 @@
+module = {
+    name: 'replace with regex',
+    description: 'replaces an old string with a new string using regular expressions',
+    categories: ['string'],
+    properties: [
+        {
+            name: 'Search Text Regex',
+            type: 'text',
+            default: '(Old)'
+        },
+        {
+            name: 'Regex Flags',
+            type: 'text',
+            default: 'g'
+        },
+        {
+            name: 'Replace Text',
+            type: 'text',
+            default: 'New $1'
+        },
+    ],
+    run: function (text, search, flags, replace) {
+        const re = new RegExp(search, flags)
+        return text.replace(re, replace);
+    }
+}
